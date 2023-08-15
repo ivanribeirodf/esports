f8b36d45965b4c0876d7b8daaadaf28e0b790bc5
IvanPix <ivan@pix.com.br>
2023-08-14 23:30:46 -0300

    Web e mobile

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,10 +2,19 @@
 export default {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background.png')",
+        'nlw-gradiente': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 30.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
   },
   plugins: [],
 }
-
